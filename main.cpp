1cf2d5022853442d7e63c8daf4a5c8203bf81024
Vadym Fedorets <fedorets.vadim@gmail.com>
2024-08-03 07:34:33 +0200

    [sentence predictor] initial phase of sentence predictor and fixed some segmentation faults

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 class KeyPredictor {
- public:
-  KeyPredictor() {
-  }
-  
-  KeyPredictor(string layout_type) {
-    if(layout_type == "QWERTY") {
+public:
+  KeyPredictor(string layout_type = "QWERTY", double starting_value = 5.0, double coeff = 0.25, int max_dstnc = 7) {
+    hit_value = starting_value;
+    coefficient = coeff;
+    max_distance = max_dstnc;
+    if (layout_type == "QWERTY") {
       connections = {
 	// Number row
 	{'1', {'2'}},
@@ -65,44 +65,52 @@ class KeyPredictor {
     cur_stage.push(given_char);
     map<char, int> distance;
     distance[given_char] = 0;
-    while(!cur_stage.empty()) {
+
+    while (!cur_stage.empty()) {
       char current_vertex = cur_stage.front();
       cur_stage.pop();
-      for(auto it:connections[current_vertex]) {
-	if(distance.find(it) == distance.end()) {
+      for (char it : connections[current_vertex]) {
+	if (distance.find(it) == distance.end()) {
 	  distance[it] = distance[current_vertex] + 1;
-	  if(distance[it] < 7) { // keys that located more than in 7 keys from pressed have too low possibility
+	  if (distance[it] < max_distance) { // keys that located more than in max_distance keys from pressed have too low possibility
 	    cur_stage.push(it);
 	  }
 	}
       }
     }
+
     vector<pair<char, double>> possibilities;
-    for(auto it:distance) {
-      if(possible_chars.find(it.first) != possible_chars.end()) {
-	possibilities.push_back({it.first, 5.0*pow(0.25, it.second)});
+    for (const auto& it : distance) {
+      if (possible_chars.find(it.first) != possible_chars.end()) {
+	possibilities.push_back({it.first, hit_value * pow(coefficient, it.second)});
       }
     }
     return possibilities;
   }
   
- private:
-  unordered_map<char, unordered_set<char> > connections;  
+private:
+  unordered_map<char, unordered_set<char>> connections;
+  double hit_value, coefficient;
+  int max_distance;
 };
 
 
 class WordPredictor {
 public:
-  WordPredictor(string layout_type) {
+  WordPredictor(string layout_type = "QWERTY", double starting_value = 5.0, double coeff = 0.25, int max_dstnc = 7) {
     root = new TrieNode();
-    key_predictor = KeyPredictor(layout_type);
+    key_predictor = KeyPredictor(layout_type, starting_value, coeff, max_dstnc);
+  }
+
+  ~WordPredictor() {
+    clearTrie(root);
   }
 
   bool insert(string word) {
     TrieNode* current_node = root;
-    for(auto it:word) {
-      if(current_node->connections[it] == NULL) {
-	current_node->connections[it] = new TrieNode();	
+    for (char it : word) {
+      if (current_node->connections.find(it) == current_node->connections.end()) {
+	current_node->connections[it] = new TrieNode();
       }
       current_node = current_node->connections[it];
     }
@@ -112,77 +120,212 @@ public:
   
   bool insert(vector<string> words) {
     bool result = true;
-    for(auto it:words) {
+    for (const auto& it : words) {
       result &= insert(it);
     }
     return result;
   }
 
-  vector<pair<string, double> > get_possibility(string users_word) {
+  vector<pair<string, double>> get_possibility(string word) {
     vector<pair<string, double>> possibilities;
-    TrieNode* current_node = root;
-    queue<pair<TrieNode*, pair<double, string> > > processing_queue;
-    processing_queue.push({root, {1, ""}});
-    for(auto key:users_word) {
+    queue<PossibilityNode*> processing_queue;
+    processing_queue.push(new PossibilityNode(root, "", 1));
+
+    for (char key : word) {
       set<char> possible_chars;
-      queue<pair<TrieNode*, pair<double, string> > > copy = processing_queue;
-      while(!copy.empty()) {	
-	for(auto pair : copy.front().first->connections) {
-	  possible_chars.insert(pair.first);
-	}
+      queue<PossibilityNode*> copy = processing_queue;
+      while (!copy.empty()) {
+	PossibilityNode* node = copy.front();
 	copy.pop();
-      }
-      vector<pair<char, double> > predicted = key_predictor.get_possibility(possible_chars, key);
-      int len = processing_queue.size();
-      for(int i = 0; i < len; i++) {
-	pair<TrieNode*, pair<double, string> > cur = processing_queue.front();
-	processing_queue.pop();
-	TrieNode* current_node = cur.first;
-	double possibility = cur.second.first;
-	string s = cur.second.second;
-	if(current_node->is_word) {
-	  possibilities.push_back({s, possibility});
+	if (node != nullptr && node->node != nullptr) {
+	  for (const auto& char_pos : node->node->connections) {
+	    possible_chars.insert(char_pos.first);
+	  }
 	}
-	for(auto it:predicted) {
-	  if(current_node->connections[it.first] != NULL) {
-	    processing_queue.push({current_node->connections[it.first], {possibility * it.second, s + it.first}});
+      }
+      vector<pair<char, double>> char_possibilities = key_predictor.get_possibility(possible_chars, key);
+      int queue_start_len = processing_queue.size();
+      for (int i = 0; i < queue_start_len; ++i) {
+	PossibilityNode* cur = processing_queue.front();
+	processing_queue.pop();
+	if (cur != nullptr && cur->node != nullptr) {
+	  if (cur->node->is_word) {
+	    possibilities.push_back({cur->content, cur->possibility});
+	  }
+	  for (const auto& it : char_possibilities) {
+	    if (cur->node->connections.find(it.first) != cur->node->connections.end()) {
+	      processing_queue.push(new PossibilityNode(cur->node->connections[it.first], cur->content + it.first, cur->possibility * it.second));
+	    }
 	  }
 	}
       }
     }
-    while(!processing_queue.empty()) {
-      auto current = processing_queue.front();
-      if(current.first->is_word) {
-	possibilities.push_back({current.second.second, current.second.first});
-      }
-      for(auto pair : current.first->connections) {
-	processing_queue.push({pair.second, {current.second.first, current.second.second + pair.first}});
-      }
+
+    while (!processing_queue.empty()) {
+      PossibilityNode* cur = processing_queue.front();
       processing_queue.pop();
+      if (cur != nullptr && cur->node != nullptr && cur->node->is_word) {
+	possibilities.push_back({cur->content, cur->possibility});
+      }
+      for (const auto& char_node : cur->node->connections) {
+	processing_queue.push(new PossibilityNode(char_node.second, cur->content + char_node.first, cur->possibility));
+      }
+      delete cur; // Clean up memory
     }
+
     return possibilities;
   }
 
 private:
   struct TrieNode {
-    TrieNode() {
-      is_word = false;
-    }
+    TrieNode() : is_word(false) {}
     unordered_map<char, TrieNode*> connections;
     bool is_word;
   };
 
+  struct PossibilityNode {
+    PossibilityNode(TrieNode* tn, string s, double pos) : node(tn), content(s), possibility(pos) {}
+    TrieNode* node;
+    string content;
+    double possibility;
+  };
+
   TrieNode* root;
   KeyPredictor key_predictor;
+
+  void clearTrie(TrieNode* node) {
+    if (node == nullptr) return;
+    for (auto& pair : node->connections) {
+      clearTrie(pair.second);
+    }
+    delete node;
+  }
+};
+
+
+class SentencePredictor {
+public:
+  SentencePredictor() {}
+
+  ~SentencePredictor() {}
+
+  bool insert(string sentence) {
+    vector<string> words = to_words(sentence);
+    TrieNode* current_node = root;
+    for (const auto& word : words) {
+      if (current_node->connections.find(word) == current_node->connections.end()) {
+	current_node->connections[word] = new TrieNode();    
+      }
+      current_node = current_node->connections[word];
+      word_predictor.insert(word);
+    }
+    current_node->is_sentence = true;
+    return true;
+  }
+
+  bool insert(vector<string> sentences) {
+    bool status = true;
+    for (const auto& sentence : sentences) {
+      status &= insert(sentence);
+    }
+    return status;
+  }
+
+  vector<pair<string, double>> get_possibility(string sentence) {
+    vector<string> words = to_words(sentence);
+    vector<pair<string, double>> possibilities;
+    queue<PossibilityNode*> processing_queue;
+    processing_queue.push(new PossibilityNode(root, "", 1));
+
+    for (const auto& word : words) {
+      set<string> possible_words;
+      queue<PossibilityNode*> copy = processing_queue;
+      while (!copy.empty()) {
+	PossibilityNode* node = copy.front();
+	copy.pop();
+	if (node != nullptr && node->node != nullptr) {
+	  for (const auto& word_pos : node->node->connections) {
+	    possible_words.insert(word_pos.first);
+	  }
+	}
+      }
+      vector<pair<string, double>> word_possibilities = word_predictor.get_possibility(word);
+      int queue_start_len = processing_queue.size();
+      for (int i = 0; i < queue_start_len; ++i) {
+	PossibilityNode* cur = processing_queue.front();
+	processing_queue.pop();
+	if (cur != nullptr && cur->node != nullptr) {
+	  if (cur->node->is_sentence) {
+	    possibilities.push_back({cur->content, cur->possibility});
+	  }
+	  for (const auto& it : word_possibilities) {
+	    if (cur->node->connections.find(it.first) != cur->node->connections.end()) {
+	      processing_queue.push(new PossibilityNode(cur->node->connections[it.first], cur->content + " " + it.first, cur->possibility * it.second));
+	    }
+	  }
+	}
+      }
+    }
+
+    while (!processing_queue.empty()) {
+      PossibilityNode* cur = processing_queue.front();
+      processing_queue.pop();
+      if (cur != nullptr && cur->node != nullptr && cur->node->is_sentence) {
+	possibilities.push_back({cur->content, cur->possibility});
+      }
+      for (const auto& char_node : cur->node->connections) {
+	processing_queue.push(new PossibilityNode(char_node.second, cur->content + " " + char_node.first, cur->possibility));
+      }
+      delete cur; // Clean up memory
+    }
+
+    return possibilities;
+  }
+  
+private:
+  struct TrieNode {
+    TrieNode() : is_sentence(false) {}
+    unordered_map<string, TrieNode*> connections;
+    bool is_sentence;
+  };
+
+  struct PossibilityNode {
+    PossibilityNode(TrieNode* tn, string s, double pos) : node(tn), content(s), possibility(pos) {}
+    TrieNode* node;
+    string content;
+    double possibility;
+  };
+
+  TrieNode* root = new TrieNode();
+  WordPredictor word_predictor;
+
+  void clearTrie(TrieNode* node) {
+    if (node == nullptr) return;
+    for (auto& pair : node->connections) {
+      clearTrie(pair.second);
+    }
+    delete node;
+  }
+
+  vector<string> to_words(string sentence) {
+    vector<string> words;
+    istringstream iss(sentence);
+
+    string word;
+    while (iss >> word) {
+      words.push_back(word);
+    }
+    return words;
+  }
 };
 
 
 int main() {
-  WordPredictor pred("QWERTY");
-  bool status = pred.insert(vector<string>({"ASD", "BSC", "QW", "QWE", "QWD", "POQ", "SWY", "QWEG", "QWEAS"}));
+  SentencePredictor pred;
+  bool status = pred.insert(vector<string>({"HOW TO FIND FRIENDS", "HOW TO FIND KEYS", "HOW TO PLAY MARIO", "HELLO", "HOWDY", "QWEQWEQW"}));
   cout << "insert status : " << status << "\n";
-  vector<pair<string, double> > a = pred.get_possibility("QWE");
-  for(auto it:a) {
+  vector<pair<string, double>> a = pred.get_possibility("HOW TI FUM");
+  for (const auto& it : a) {
     cout << it.first << " " << it.second << "\n";
   }
   return 0;
